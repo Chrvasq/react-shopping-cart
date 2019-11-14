5a93c48e75afba3b1f4e03979defa5f9b11d0689
Chris Vasquez <chrvasq@outlook.com>
2019-11-13 21:19:23 -0800

    Add removeItem functionality

--- src/App.js
+++ src/App.js
@@ -20,13 +20,17 @@ function App() {
     setCart(cart => [...cart, item]);
   };
 
+  const removeItem = itemID => {
+    setCart(cart.filter(cartItem => cartItem.id !== Number(itemID)));
+  };
+
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <CartContext.Provider value={cart}>
+        <CartContext.Provider value={{ cart, removeItem }}>
           <Navigation />
           {/* Routes */}
-          <Route exact path="/" component={Products} /> />
+          <Route exact path="/" component={Products} />
           <Route path="/cart" component={ShoppingCart} />
         </CartContext.Provider>
       </ProductContext.Provider>
