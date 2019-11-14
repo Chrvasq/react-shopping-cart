d2e0e04c84a17568bff6659e636b6d16e5b81add
Chris Vasquez <chrvasq@outlook.com>
2019-11-13 23:54:19 -0800

    Add shopping cart persistance

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import data from "./data";
 
@@ -11,9 +11,14 @@ import ShoppingCart from "./components/ShoppingCart";
 import { ProductContext } from "./contexts/ProductContext";
 import { CartContext } from "./contexts/CartContext";
 
+//custom hook
+import useLocalStorage from "./hooks/useLocalStorage";
+
 function App() {
   const [products] = useState(data);
   const [cart, setCart] = useState([]);
+  // sets persistCart value to value returned by useLocalStorage hook
+  const [persistCart, setPersistCart] = useLocalStorage("cart", []);
 
   const addItem = item => {
     // add the given item to the cart
@@ -23,6 +28,16 @@ function App() {
   const removeItem = itemID => {
     setCart(cart.filter(cartItem => cartItem.id !== Number(itemID)));
   };
+
+  // will set cart state to persistCart state value on initial mount
+  useEffect(() => {
+    setCart(persistCart);
+  }, []);
+
+  // will set persistCart state to current cart value when cart state changes
+  useEffect(() => {
+    setPersistCart(cart);
+  }, [cart]);
 
   return (
     <div className="App">
